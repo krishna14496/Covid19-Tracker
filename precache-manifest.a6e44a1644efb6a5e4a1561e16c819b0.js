2daa76d25ea98ab6dc5d6a845f7360e6c957e47a
Pandey <ashish.pandey@capgemini.com>
2020-05-01 06:22:44 +0530

    Updates

---
+++ precache-manifest.a6e44a1644efb6a5e4a1561e16c819b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41e520680f1e7c183bc66f48b429066",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "1aeeb875ee1483899334",
+    "url": "/Covid19-Tracker/static/css/main.0f5ecea8.chunk.css"
+  },
+  {
+    "revision": "d3acfc9cc4e820d2ca52",
+    "url": "/Covid19-Tracker/static/js/2.83213221.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-Tracker/static/js/2.83213221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aeeb875ee1483899334",
+    "url": "/Covid19-Tracker/static/js/main.332ebbbf.chunk.js"
+  },
+  {
+    "revision": "e34584d7c473c27d32ad",
+    "url": "/Covid19-Tracker/static/js/runtime-main.0bc7d724.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19-Tracker/static/media/image.d7265326.png"
+  }
+]);
